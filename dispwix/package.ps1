264b9981a46472d2f3d3b430e34c6352cb98778f
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2025-04-12 03:23:33 +0100

    MSI builder with WiX

---
+++ dispwix/package.ps1
@@ -0,0 +1,84 @@
+# Copyright (c) 2025 Roger Brown.
+# Licensed under the MIT License.
+
+param(
+	$CertificateThumbprint = '601A8B683F791E51F647D34AD102C38DA4DDB65F',
+	$Architectures = @('arm', 'arm64', 'x86', 'x64')
+)
+
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+
+trap
+{
+	throw $PSItem
+}
+
+foreach ($ARCH in $Architectures)
+{
+	$VERSION=(Get-Item -LiteralPath "..\dispsvr\bin\Release\net481\win-$ARCH\publish\dispsvr.exe").VersionInfo.ProductVersion
+
+	$env:PRODUCTVERSION = $VERSION
+	$env:PRODUCTARCH = $ARCH
+	$env:PRODUCTWIN64 = 'yes'
+	$env:PRODUCTPROGFILES = 'ProgramFiles64Folder'
+	$env:INSTALLERVERSION = '500'
+
+	switch ($ARCH)
+	{
+		'arm64' {
+			$env:UPGRADECODE = 'BDBCEF05-DC81-45A4-87C8-5BBC229B76A8'
+		}
+
+		'arm' {
+			$env:UPGRADECODE = '0F3AFA41-2B8E-4108-86EF-8A50798BAB2F'
+			$env:PRODUCTWIN64 = 'no'
+			$env:PRODUCTPROGFILES = 'ProgramFilesFolder'
+		}
+
+		'x86' {
+			$env:UPGRADECODE = '264D0E52-1F16-408C-ADDE-2E4EFD8D2024'
+			$env:PRODUCTWIN64 = 'no'
+			$env:PRODUCTPROGFILES = 'ProgramFilesFolder'
+			$env:INSTALLERVERSION = '200'
+		}
+
+		'x64' {
+			$env:UPGRADECODE = '88E8BA10-CB38-4EED-A8CD-DBBE9FC2B182'
+			$env:INSTALLERVERSION = '200'
+		}
+	}	
+
+	& "${env:WIX}bin\candle.exe" -nologo "dispsvr.wxs"
+
+	if ($LastExitCode -ne 0)
+	{
+		exit $LastExitCode
+	}
+
+	$MsiFilename = "rhubarb-geek-nz.RegistrationServices-$VERSION-$ARCH.msi"
+
+	& "${env:WIX}bin\light.exe" -nologo -cultures:null -out $MsiFilename 'dispsvr.wixobj'
+
+	if ($LastExitCode -ne 0)
+	{
+		exit $LastExitCode
+	}
+
+	Remove-Item 'dispsvr.wix*'
+	Remove-Item 'rhubarb-geek-nz.RegistrationServices-*.wixpdb'
+
+	if ($CertificateThumbprint)
+	{
+		$codeSignCertificate = Get-ChildItem -path Cert:\ -Recurse -CodeSigningCert | Where-Object { $_.Thumbprint -eq $CertificateThumbprint }
+
+		if ($codeSignCertificate -and ($codeSignCertificate.Count -eq 1))
+		{
+			$result = Set-AuthenticodeSignature -FilePath $MsiFilename -HashAlgorithm 'SHA256' -Certificate $codeSignCertificate -TimestampServer 'http://timestamp.digicert.com'
+		}
+		else
+		{
+			Write-Error "Error with certificate - $CertificateThumbprint"
+		}
+	}
+}
